eb94c736501bae26a6bb5e7847766e9d52c68281
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-08 19:36:44 +0000

    added nodemon

--- express_server.js
+++ express_server.js
@@ -12,9 +12,9 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -26,7 +26,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL };
   res.render("urls_show", templateVars)
 });
 
@@ -34,24 +34,34 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body> Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(`${longURL}`)
 });
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body> Hello <b>World</b></body></html>\n");
+// });
+
+// creates 6-digit string and pushes updated values to long/shortURL
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let newURL = generateRandomString();
+  urlDatabase[newURL] = {longURL: req.body.longURL};
+  res.redirect(`/urls/${newURL}`);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+// helper functions
 function generateRandomString() {
   let randomStr = '';
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 7; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  for (let i = 0; i <= 6; i++) {
+    randomStr += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return randomStr;
 };
